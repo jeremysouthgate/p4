196fc3626d2f11f5aa80f3769a89559594eacf99
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-05-15 14:20:36 -0400

    update seeder jill/jamal

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -23,7 +23,7 @@ class UserTableSeeder extends Seeder
         // Test User: Jill
         $salt = uniqid();
         $user = new User();
-        $user->email = "jill@g.harvard.edu";
+        $user->email = "jill@harvard.edu";
         $user->salt = $salt;
         $user->hash = Hash::make($salt."helloworld");
         $user->activation_status = "active";
@@ -32,7 +32,7 @@ class UserTableSeeder extends Seeder
         // Test User: Jamal
         $salt = uniqid();
         $user = new User();
-        $user->email = "jamal@g.harvard.edu";
+        $user->email = "jamal@harvard.edu";
         $user->salt = $salt;
         $user->hash = Hash::make($salt."helloworld");
         $user->activation_status = "active";
